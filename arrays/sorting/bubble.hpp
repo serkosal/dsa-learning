9fe3ca39f282393ac81c4fba7f355696b21c41fe
Sergey Kosykh <s3rkosal@gmail.com>
2026-06-01 14:45:58 +0400

    rewrite bubble using iterators

--- arrays/sorting/bubble.hpp
+++ arrays/sorting/bubble.hpp
@@ -1,19 +1,8 @@
 #include "../helpers.hpp"
 
-void bubble_sort(V& vec) {
-    size_t v_size = vec.size();
-    if (v_size == 0 || v_size == 1) return; 
-
-    for (size_t end_index = v_size - 1; end_index != 0; --end_index)
-        for (size_t i = 0; i != end_index; ++i)
-            if (vec[i] > vec[i + 1])
-                std::swap(vec[i], vec[i + 1]);
+void bubble_sort(V& v) {
+    for (auto last = v.end() - 1; last != v.begin(); --last)
+        for (auto cur = v.begin(); cur != last; ++cur)
+            if (*cur > *(cur + 1))
+                std::swap(*cur, *(cur + 1));
 }
-
-// void cocktail_sort(V& vec) {
-    
-// }
-
-// void comb_sort(V& vec) {
-    
-// }
